f83e809d4883ae98aa0fb711f763a54a380c324a
mys9ys <rjbexa@yandex.ru>
2022-01-24 12:57:40 +0300

    setting расширение

--- core/base/controllers/RouteController.php
+++ core/base/controllers/RouteController.php
@@ -2,23 +2,36 @@
 
 namespace core\base\controllers;
 
+use core\base\settings\Settings;
+use core\base\settings\SiteSettings;
+
 class RouteController
 {
     static private $_instance;
-
-    private function __construct()
-    {
-    }
 
     private function __clone()
     {
     }
 
-    static public function getInstace(){
+    static public function getInstance(){
         if(self::$_instance instanceof self){
             return self::$_instance;
         }
 
         return self::$_instance = new self;
+    }
+
+    private function __construct()
+    {
+        $s = Settings::get('routes');
+        $sp = SiteSettings::get('routes');
+//        $sp = SiteSettings::get('templateArr');
+        $s1 = $s['admin'];
+
+
+        echo "<pre>";
+        var_dump($sp);
+        echo "</pre>";
+
     }
 }
--- index.php
+++ index.php
@@ -7,13 +7,19 @@ session_start();
 
 require_once 'config.php';
 require_once 'core/base/settings/internal_settings.php';
+require_once 'lib/functions.php';
 
 use core\base\controllers\RouteController;
 use core\base\exceptions\RouteException;
 
 try{
-    RouteController::getInstance()->route();
+//    RouteController::getInstance()->route();
+    RouteController::getInstance();
 }
 catch (RouteException $e){
     exit($e->getMessage());
 }
+
+$arr = [1,2,3];
+
+print_debug($arr);
