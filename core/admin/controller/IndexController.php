57cc45565225fcf79aeb951971080633b520dee9
Mys9ys9ka <rjbexa@yandex.ru>
2022-02-01 18:41:22 +0300

    метод создания инструкции JOIN (1)

--- core/admin/controller/IndexController.php
+++ core/admin/controller/IndexController.php
@@ -22,7 +22,34 @@ class IndexController extends BaseController
             'condition' => ['AND'],
             'order' => ['fio', 'name'],
             'order_direction' => ['DESC'],
-            'limit' => '1'
+            'limit' => '1',
+            'join' => [
+                'join_table1' => [
+                    'table'=> 'join_table1',
+                    'fields' => ['id as j_id'. 'name as j_name'],
+                    'type'=> 'left',
+                    'where' => ['name'=> 'masha'],
+                    'operand' => ['='],
+                    'condition' => ['OR'],
+//                    'on'=>  ['id', 'parent_id']
+                    'on'=> [
+                        'table' => 'teachers',
+                        'fields' => ['id', 'parent_id']
+                    ]
+                ],
+                'join_table2' => [
+                    'table'=> 'join_table2',
+                    'fields' => ['id as j2_id'. 'name as j2_name'],
+                    'type'=> 'left',
+                    'where' => ['name'=> 'masha'],
+                    'operand' => ['='],
+                    'condition' => ['OR'],
+                    'on'=> [
+                        'table' => 'teachers',
+                        'fields' => ['id', 'parent_id']
+                    ]
+                ]
+            ]
         ]);
 
         exit('admin');
