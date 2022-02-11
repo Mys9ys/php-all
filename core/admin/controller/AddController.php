c2571497004b8242fc10e4da6d80e9b14dd1670a
mys9ys <rjbexa@yandex.ru>
2022-02-11 12:29:49 +0300

    commit merge

--- core/admin/controller/AddController.php
+++ core/admin/controller/AddController.php
@@ -21,27 +21,27 @@ class AddController extends BaseAdmin
 
         $this->createOutputData();
 
-        $this->manyAdd();
+//        $this->manyAdd();
 
-        exit;
+//        exit;
 
     }
 
-    protected function manyAdd(){
-
-            $fields = [
-//                ['name' => 'Lena', 'img' => '2.jpg', 'menu_position' => 1],
-                'name' => 'Lena', 'menu_position' => '1',
-//                ['name' => 'Olya', 'img' => '3.jpg'],
-            ];
-
-            $files = ['img' => ['1.jpg', '4.jpg']];
-
-            $this->model->add('teachers', [
-                'fields' => $fields,
-                'files' => $files
-            ]);
-    }
+//    protected function manyAdd(){
+//
+//            $fields = [
+////                ['name' => 'Lena', 'img' => '2.jpg', 'menu_position' => 1],
+//                'name' => 'Lena', 'menu_position' => '1',
+////                ['name' => 'Olya', 'img' => '3.jpg'],
+//            ];
+//
+//            $files = ['img' => ['1.jpg', '4.jpg']];
+//
+//            $this->model->add('teachers', [
+//                'fields' => $fields,
+//                'files' => $files
+//            ]);
+//    }
 
     protected function createForeignProperty($arr, $rootItems)
     {
