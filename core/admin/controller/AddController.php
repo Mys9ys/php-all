307a7a47ca4152e3ba8c73062b1322be439133b7
mys9ys <rjbexa@yandex.ru>
2022-02-11 14:50:47 +0300

    подключение шаблона добавления и редактирования данных

--- core/admin/controller/AddController.php
+++ core/admin/controller/AddController.php
@@ -6,6 +6,7 @@ use core\base\settings\Settings;
 
 class AddController extends BaseAdmin
 {
+    protected $action = 'add';
 
     protected function inputData()
     {
@@ -21,27 +22,7 @@ class AddController extends BaseAdmin
 
         $this->createOutputData();
 
-//        $this->manyAdd();
-
-//        exit;
-
     }
-
-//    protected function manyAdd(){
-//
-//            $fields = [
-////                ['name' => 'Lena', 'img' => '2.jpg', 'menu_position' => 1],
-//                'name' => 'Lena', 'menu_position' => '1',
-////                ['name' => 'Olya', 'img' => '3.jpg'],
-//            ];
-//
-//            $files = ['img' => ['1.jpg', '4.jpg']];
-//
-//            $this->model->add('teachers', [
-//                'fields' => $fields,
-//                'files' => $files
-//            ]);
-//    }
 
     protected function createForeignProperty($arr, $rootItems)
     {
--- core/admin/controller/BaseAdmin.php
+++ core/admin/controller/BaseAdmin.php
@@ -26,6 +26,9 @@ abstract class BaseAdmin extends BaseController
     protected $translate;
     protected $blocks = [];
 
+    protected $templateArr;
+    protected $formTemplates;
+
 
     protected function inputData()
     {
@@ -37,6 +40,9 @@ abstract class BaseAdmin extends BaseController
         if (!$this->model) $this->model = Model::instance();
         if (!$this->menu) $this->menu = Settings::get('projectTables');
         if (!$this->adminPath) $this->adminPath = PATH . Settings::get('routes')['admin']['alias'] . '/';
+
+        if(!$this->templateArr) $this->templateArr = Settings::get('templateArr');
+        if(!$this->formTemplates) $this->formTemplates = Settings::get('formTemplates');
 
         $this->sendNoCacheHeaders();
 
