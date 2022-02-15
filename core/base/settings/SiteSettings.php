43a9d51c17b9b869e215d82f5b41a8519fc3f9f6
mys9ys <rjbexa@yandex.ru>
2022-02-15 18:39:05 +0300

    создание чпу страниц

--- core/base/settings/SiteSettings.php
+++ core/base/settings/SiteSettings.php
@@ -2,14 +2,10 @@
 
 namespace core\base\settings;
 
-use core\base\controller\SingleTon;
-use core\base\settings\Settings;
-
 class SiteSettings extends Settings
 {
-    use SingleTon {
-        instance as traitInstance;
-    }
+
+    use BaseSettings;
 
     private $baseSettings;
 
@@ -19,35 +15,16 @@ class SiteSettings extends Settings
     ];
 
     private $routes = [
-//        'admin' => [
-//            'name' => 'sudo',
-//        ],
+        'plugins' => [
+            'dir' => 'dir',
+            'routes' => [
+                'route1' => ['1', '2']
+            ]
+        ],
     ];
 
 //    private $expansion = 'core/plugin/expansion/';
 
-    static public function get($property){
-        return self::instance()->$property;
-    }
 
-    static private function instance(){
-        if(self::$_instance instanceof self){
-            return self::$_instance;
-        }
-
-        self::traitInstance()->baseSettings = Settings::instance();
-        $baseProperties = self::$_instance->baseSettings->clueProperties(get_class());
-        self::$_instance->setProperty($baseProperties);
-
-        return self::$_instance;
-    }
-
-    protected function setProperty($properties){
-        if($properties){
-            foreach ($properties as $name=>$property){
-                $this->$name=$property;
-            }
-        }
-    }
 
 }
