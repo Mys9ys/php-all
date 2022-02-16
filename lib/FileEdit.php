e5e872ae79696336f5a27575dc508bb2fbd2b82e
mys9ys <rjbexa@yandex.ru>
2022-02-16 12:30:34 +0300

    класс добавления файлов на сервер (2)

--- lib/FileEdit.php
+++ lib/FileEdit.php
@@ -11,7 +11,7 @@ class FileEdit
     public function addFile($directory = false)
     {
 
-        if ($directory) $this->directory = $_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR;
+        if (!$directory) $this->directory = $_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR;
         else $this->directory = $directory;
 
         foreach ($_FILES as $key => $file) {
@@ -37,7 +37,7 @@ class FileEdit
                 }
 
             } else {
-                if ($file['name']) {                  
+                if ($file['name']) {
 
                     $res_name = $this->createFile($file);
 
@@ -49,7 +49,45 @@ class FileEdit
         return $this->getFiles();
     }
 
-    public function getFiles(){
+    protected function createFile($file)
+    {
+
+        $fileNameArr = explode('.', $file['name']);
+        $ext = $fileNameArr[count($fileNameArr) - 1];
+        unset($fileNameArr[count($fileNameArr) - 1]);
+
+        $fileName = implode('.', $fileNameArr);
+
+        $fileName = (new TextModify())->translit($fileName);
+
+        $fileName = $this->checkFile($fileName, $ext);
+
+        $fileFullName = $this->directory . $fileName;
+
+        if ($this->uploadFile($file['tmp_name'], $fileFullName))
+            return $fileName;
+
+        return false;
+
+    }
+
+    protected function uploadFile($tmp_name, $dest)
+    {
+        if (move_uploaded_file($tmp_name, $dest)) return true;
+
+        return false;
+    }
+
+    protected function checkFile($fileName, $ext, $fileLastName = '')
+    {
+        if (!file_exists($this->directory . $fileName . $fileLastName . '.' . $ext))
+            return $fileName . $fileLastName . '.' . $ext;
+
+        return $this->checkFile($fileName, $ext, '_' . hash('crc32', time() . mt_rand(1, 100)));
+    }
+
+    public function getFiles()
+    {
         return $this->imgArr;
     }
 }
