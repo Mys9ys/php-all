a995afaa20a81b07e8e1230729e3bc5239c11bca
Mys9ys9ka <rjbexa@yandex.ru>
2022-02-20 18:33:21 +0300

    парсер - исправление ошибки

--- core/admin/controller/CreateSitemapController.php
+++ core/admin/controller/CreateSitemapController.php
@@ -25,20 +25,22 @@ class CreateSitemapController extends BaseAdmin
             $this->writeLog('Отсутствует библиотека CURL');
             $_SESSION['res']['answer'] = '<div class="error">Отсутствует библиотека CURL</div>';
             $this->redirect();
-
-            set_time_limit(0);
-
-            if (file_exists($_SERVER['DOCUMENT_ROOT'] . PATH . 'log/' . $this->parsingLogFile))
-                @unlink($_SERVER['DOCUMENT_ROOT'] . PATH . 'log/' . $this->parsingLogFile);
-
-            $this->parsing(SITE_URL);
-
-            $this->createSitemap();
-
-            !$_SESSION['res']['answer'] && $_SESSION['res']['answer'] = '<div class="success">Sitemap is created</div>';
-
-            $this->redirect();
         }
+
+        set_time_limit(0);
+
+        if (file_exists($_SERVER['DOCUMENT_ROOT'] . PATH . 'log/' . $this->parsingLogFile))
+            @unlink($_SERVER['DOCUMENT_ROOT'] . PATH . 'log/' . $this->parsingLogFile);
+
+        $this->parsing(SITE_URL);
+
+
+        $this->createSitemap();
+
+        !$_SESSION['res']['answer'] && $_SESSION['res']['answer'] = '<div class="success">Sitemap is created</div>';
+
+        $this->redirect();
+
 
     }
 
